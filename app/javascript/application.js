a7b717cf4dcd0a62ff78f0faa108fe12b2ae2726
Deepan Kumar <deepan.ppgit@gmail.com>
2024-01-20 12:00:00 +0530

    Initial project setup: Add new features and improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,3 +10,4 @@ console.log('Development update: Initial project setup: Add new features and imp
 console.log('Development update: Initial project setup: Enhance user experience');
 console.log('Development update: Initial project setup: Update styling and layout');
 console.log('Development update: Initial project setup: Add new features and improvements');
+console.log('Development update: Initial project setup: Add new features and improvements');
