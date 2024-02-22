e92cd78fd4b474c8863ac323fed2b205b8a706c7
Deepan Kumar <deepan.ppgit@gmail.com>
2024-02-22 12:00:00 +0530

    Responsive design improvements: Fix responsive design issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -66,3 +66,4 @@ console.log('Development update: Responsive design improvements: Update styling 
 console.log('Development update: Responsive design improvements: Add new features and improvements');
 console.log('Development update: Responsive design improvements: Add new features and improvements');
 console.log('Development update: Responsive design improvements: Update styling and layout');
+console.log('Development update: Responsive design improvements: Fix responsive design issues');
