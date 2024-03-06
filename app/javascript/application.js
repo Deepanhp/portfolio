cc151ecf34b5278db7ff92a48c9b48f3cca43d7f
Deepan Kumar <deepan.ppgit@gmail.com>
2024-03-06 12:00:00 +0530

    QR code feature: Optimize performance

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -84,3 +84,4 @@ console.log('Development update: Animation additions: Enhance user experience');
 console.log('Development update: QR code feature: Enhance user experience');
 console.log('Development update: QR code feature: Optimize performance');
 console.log('Development update: QR code feature: Add new features and improvements');
+console.log('Development update: QR code feature: Optimize performance');
