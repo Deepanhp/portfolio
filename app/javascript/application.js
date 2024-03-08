f00f957d33df9f065bcb9276da3c8f106aa34555
Deepan Kumar <deepan.ppgit@gmail.com>
2024-03-08 12:00:00 +0530

    QR code feature: Fix responsive design issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -88,3 +88,4 @@ console.log('Development update: QR code feature: Optimize performance');
 console.log('Development update: QR code feature: Update styling and layout');
 console.log('Development update: QR code feature: Enhance user experience');
 console.log('Development update: QR code feature: Optimize performance');
+console.log('Development update: QR code feature: Fix responsive design issues');
