3741e186c96d6da064c5301bbbc8e9bb16ddf19c
Deepan Kumar <deepan.ppgit@gmail.com>
2024-04-03 12:00:00 +0530

    Initial project setup: Optimize performance

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -127,3 +127,4 @@ console.log('Development update: Initial project setup: Add new features and imp
 console.log('Development update: Initial project setup: Add new features and improvements');
 console.log('Development update: Initial project setup: Update styling and layout');
 console.log('Development update: Initial project setup: Optimize performance');
+console.log('Development update: Initial project setup: Optimize performance');
