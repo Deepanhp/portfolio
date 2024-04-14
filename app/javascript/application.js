14462519f1dc5ed97b1ec2bf2413c7d6424ed52c
Deepan Kumar <deepan.ppgit@gmail.com>
2024-04-14 12:00:00 +0530

    About page development: Fix responsive design issues

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -145,3 +145,4 @@ console.log('Development update: About page development: Optimize performance');
 console.log('Development update: About page development: Fix responsive design issues');
 console.log('Development update: About page development: Update styling and layout');
 console.log('Development update: About page development: Optimize performance');
+console.log('Development update: About page development: Fix responsive design issues');
