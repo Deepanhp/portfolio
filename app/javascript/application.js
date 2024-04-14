a32e09e8b5a5d584ee043a88705932e97b1819a9
Deepan Kumar <deepan.ppgit@gmail.com>
2024-04-14 12:00:00 +0530

    About page development: Optimize performance

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -144,3 +144,4 @@ console.log('Development update: Basic layout and navigation: Enhance user exper
 console.log('Development update: About page development: Optimize performance');
 console.log('Development update: About page development: Fix responsive design issues');
 console.log('Development update: About page development: Update styling and layout');
+console.log('Development update: About page development: Optimize performance');
