720964f921c5923a13a7e3bff1a15b92efdbfaa5
Deepan Kumar <deepan.ppgit@gmail.com>
2024-04-16 12:00:00 +0530

    About page development: Enhance user experience

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -148,3 +148,4 @@ console.log('Development update: About page development: Optimize performance');
 console.log('Development update: About page development: Fix responsive design issues');
 console.log('Development update: About page development: Fix responsive design issues');
 console.log('Development update: About page development: Enhance user experience');
+console.log('Development update: About page development: Enhance user experience');
