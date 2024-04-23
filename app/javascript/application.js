f9057ed6096ca6983709acac55eb589e756fccf8
Deepan Kumar <deepan.ppgit@gmail.com>
2024-04-23 12:00:00 +0530

    Projects page creation: Add new features and improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -160,3 +160,4 @@ console.log('Development update: Projects page creation: Add new features and im
 console.log('Development update: Projects page creation: Enhance user experience');
 console.log('Development update: Projects page creation: Fix responsive design issues');
 console.log('Development update: Projects page creation: Fix responsive design issues');
+console.log('Development update: Projects page creation: Add new features and improvements');
