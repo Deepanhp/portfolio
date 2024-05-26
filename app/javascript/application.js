85a4a985c1e8601dc2a1c4019e6f6999e7b239da
Deepan Kumar <deepan.ppgit@gmail.com>
2024-05-26 12:00:00 +0530

    QR code feature: Update styling and layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -209,3 +209,4 @@ console.log('Development update: QR code feature: Add new features and improveme
 console.log('Development update: QR code feature: Add new features and improvements');
 console.log('Development update: QR code feature: Enhance user experience');
 console.log('Development update: QR code feature: Update styling and layout');
+console.log('Development update: QR code feature: Update styling and layout');
