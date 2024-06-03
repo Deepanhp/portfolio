6b39ab48444697c6e9915ac8a77e2ed87504e75a
Deepan Kumar <deepan.ppgit@gmail.com>
2024-06-03 12:00:00 +0530

    Performance optimizations: Optimize performance

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -218,3 +218,4 @@ console.log('Development update: Performance optimizations: Enhance user experie
 console.log('Development update: Performance optimizations: Optimize performance');
 console.log('Development update: Performance optimizations: Fix responsive design issues');
 console.log('Development update: Performance optimizations: Add new features and improvements');
+console.log('Development update: Performance optimizations: Optimize performance');
